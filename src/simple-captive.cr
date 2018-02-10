b616b9816dec0ef3b389c88b43de646d7f31cf9c
Ziyang <halczy@gmail.com>
2018-02-10 20:39:21 +0800

    Initialize project

---
+++ src/simple-captive.cr
@@ -0,0 +1,6 @@
+require "./simple-captive/*"
+
+# TODO: Write documentation for `Simple::Captive`
+module Simple::Captive
+  # TODO: Put your code here
+end
---
+++ src/simple-captive/version.cr
@@ -0,0 +1,3 @@
+module Simple::Captive
+  VERSION = "0.1.0"
+end
